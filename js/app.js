c3f60e65ca75cea8860b9fbc200eef99573ea567
Cocaine97 <cocaine31197@gmail.com>
2017-04-25 21:13:17 +0530

    Defined Enemies initial positions but canvas not appearing

--- js/app.js
+++ js/app.js
@@ -1,35 +1,51 @@
 // Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+var defStartX = 30;
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
+var Enemy = function(posX,posY) {
+    this.x = posX;
+    this.y = posY;
+    // Making speed random here so game doesn't get more boring than it already is :P
+    this.speedX = Math.random();
+
     this.sprite = 'images/enemy-bug.png';
 };
+
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+    this.speedX *= dt;
+    if(this.x > 500)
+    {
+        this.x = defStartX;
+    }
 };
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    this.x += 1;
+
 };
 
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var allEnemies = [new Enemy(30,60)];
+var Slayer = function(posX,posY)
+{
+    this.x = posX;
+    this.y = posY;
+    this.sprite = 'images/char-horn-girl.png';
+};
+
+
+var player = new Slayer(200,400);
 
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-
 
 
 // This listens for key presses and sends the keys to your
